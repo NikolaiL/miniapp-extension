7cab88db939fedfbb37d2ad29e5f0464996d36a9
Nikolai <nikolai1973@gmail.com>
2025-07-31 19:15:17 +0200

    fix for wagmiconnectors

--- extension/packages/nextjs/services/web3/wagmiConfig.tsx.args.mjs
+++ extension/packages/nextjs/services/web3/wagmiConfig.tsx.args.mjs
@@ -2,7 +2,10 @@
 // Reference the template file that will use this file: https://github.com/scaffold-eth/create-eth/blob/main/templates/base/packages/nextjs/services/web3/wagmiConfig.tsx.template.mjs
 
 // Default args:
-export const preContent = `import { farcasterMiniApp as miniAppConnector } from "@farcaster/miniapp-wagmi-connector";`;
+export const preContent = `
+import { farcasterMiniApp as miniAppConnector } from "@farcaster/miniapp-wagmi-connector";
+`;
+
 export const configOverrides = {
-  connectors: `[miniAppConnector(), ...$$wagmiConnectors$$]`,
+  connectors: `$$[miniAppConnector(), ...$$wagmiConnectors$$]$$`,
 };
