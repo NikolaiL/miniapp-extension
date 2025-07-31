142f4fd64f70da5be74843c0707edddac9081fab
Nikolai <nikolai1973@gmail.com>
2025-07-31 20:28:13 +0200

    miniapp provider fix

--- extension/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx.args.mjs
+++ extension/packages/nextjs/components/ScaffoldEthAppWithProviders.tsx.args.mjs
@@ -1,9 +1,7 @@
 export const preContent = `
-    import { MiniappProvider } from "./MiniappProvider";
+import { MiniappProvider } from "./MiniappProvider";
 `;
 
 export const extraProviders = {
-    'Miniapp.Provider': {
-        value: "MiniappProvider",
-    }
+    'MiniappProvider'
 };
