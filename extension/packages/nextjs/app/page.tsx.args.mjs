a3b0344ffafe4e2e2beae8766f8f44aed0628447
Nikolai <nikolai1973@gmail.com>
2025-07-31 18:42:11 +0200

    Initial commit of miniapp extension

---
+++ extension/packages/nextjs/app/page.tsx.args.mjs
@@ -0,0 +1,25 @@
+// Reference the example args file: https://github.com/scaffold-eth/create-eth-extensions/blob/example/extension/packages/nextjs/app/page.tsx.args.mjs
+// Reference the template file that will use this file: https://github.com/scaffold-eth/create-eth/blob/main/templates/base/packages/nextjs/app/page.tsx.template.mjs
+
+// Default args:
+export const preContent = `import { sdk } from "@farcaster/miniapp-sdk";`;
+export const description = `
+<p className="text-center text-lg">
+  Get started by editing{" "}
+  <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+    packages/nextjs/app/page.tsx
+  </code>
+</p>
+<p className="text-center text-lg">
+  Edit your smart contract{" "}
+  <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+    YourContract.sol
+  </code>{" "}
+  in{" "}
+  <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
+    packages/hardhat/contracts
+  </code>
+</p>
+`;
+export const externalExtensionName = "miniapp";
+export const fullContentOverride = "";
---
+++ extension/packages/nextjs/services/web3/wagmiConfig.tsx.args.mjs
@@ -0,0 +1,8 @@
+// Reference the example args file: https://github.com/scaffold-eth/create-eth-extensions/blob/example/extension/packages/nextjs/services/web3/wagmiConfig.tsx.args.mjs
+// Reference the template file that will use this file: https://github.com/scaffold-eth/create-eth/blob/main/templates/base/packages/nextjs/services/web3/wagmiConfig.tsx.template.mjs
+
+// Default args:
+export const preContent = `import { farcasterMiniApp as miniAppConnector } from "@farcaster/miniapp-wagmi-connector";`;
+export const configOverrides = {
+  connectors: [miniAppConnector(), ...wagmiConnectors],
+};
