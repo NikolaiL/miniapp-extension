d9c42c15c4aff9fc05b12f59c025ef328aa3e1fb
Nikolai <nikolai1973@gmail.com>
2025-07-31 19:42:44 +0200

    fix for .env.example

---
+++ extension/packages/nextjs/.env.example.args.mjs
@@ -0,0 +1,25 @@
+export const additionalVars: `# essential miniapp related variables
+NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME=
+NEXT_PUBLIC_URL=
+NEXT_PUBLIC_ONCHAINKIT_API_KEY=
+
+# essential miniapp manifest related  variables
+# you can get create them at https://farcaster.xyz/~/developers/mini-apps/manifest
+# required for production
+FARCASTER_HEADER=
+FARCASTER_PAYLOAD=
+FARCASTER_SIGNATURE=
+
+# optional miniapp related variables
+NEXT_PUBLIC_APP_ICON=
+NEXT_PUBLIC_APP_SUBTITLE=
+NEXT_PUBLIC_APP_DESCRIPTION=
+NEXT_PUBLIC_APP_SPLASH_IMAGE=
+NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR=
+NEXT_PUBLIC_APP_PRIMARY_CATEGORY=
+NEXT_PUBLIC_APP_HERO_IMAGE=
+NEXT_PUBLIC_APP_TAGLINE=
+NEXT_PUBLIC_APP_OG_TITLE=
+NEXT_PUBLIC_APP_OG_DESCRIPTION=
+NEXT_PUBLIC_APP_OG_IMAGE=
+`;
