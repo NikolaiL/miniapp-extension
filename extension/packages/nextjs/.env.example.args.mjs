128c8ac423a2171b86fd22d75a3f648e486192d5
Nikolai <nikolai1973@gmail.com>
2025-10-18 19:33:24 +0700

    fix for .env template

--- extension/packages/nextjs/.env.example.args.mjs
+++ extension/packages/nextjs/.env.example.args.mjs
@@ -1,7 +1,7 @@
 export const additionalVars = `
 # essential miniapp related variables
 NEXT_PUBLIC_APP_NAME='Scaffold-ETH 2 MiniApp'
-NEXT_PUBLIC_URL=https://mtt.ngrok.dev/
+NEXT_PUBLIC_URL=https://mtt.ngrok.dev
 # essential miniapp manifest related  variables
 # you can get create them at https://farcaster.xyz/~/developers/mini-apps/manifest
 # required for production
@@ -11,16 +11,16 @@ FARCASTER_SIGNATURE=yyyVPpX63FAOjqUENJN2L5LmNWHLjet/2f9ap6q7/kwkM3io/oi+CTpl8iq/
 
 
 # optional miniapp related variables
-NEXT_PUBLIC_APP_ICON=https://mtt.ngrok.dev/favicon.png
+NEXT_PUBLIC_APP_ICON=\${NEXT_PUBLIC_URL}/favicon.png
 NEXT_PUBLIC_APP_SUBTITLE="Built with Scaffold ETH2"
 NEXT_PUBLIC_APP_DESCRIPTION="Built with 🏗 Scaffold-ETH 2 and MiniApp Extension"
-NEXT_PUBLIC_APP_SPLASH_IMAGE=https://mtt.ngrok.dev/favicon.png
+NEXT_PUBLIC_APP_SPLASH_IMAGE=\${NEXT_PUBLIC_URL}/favicon.png
 NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR="#212638"
 NEXT_PUBLIC_APP_PRIMARY_CATEGORY=developer-tools
-NEXT_PUBLIC_APP_HERO_IMAGE=https://mtt.ngrok.dev/thumbnail.jpg
+NEXT_PUBLIC_APP_HERO_IMAGE=\${NEXT_PUBLIC_URL}/thumbnail.jpg
 NEXT_PUBLIC_APP_TAGLINE="Built with Scaffold ETH 2"
 NEXT_PUBLIC_APP_OG_TITLE="Scaffold-ETH 2 MiniApp"
 NEXT_PUBLIC_APP_OG_DESCRIPTION="Built with 🏗 Scaffold-ETH 2 and MiniApp Extension"
-NEXT_PUBLIC_APP_OG_IMAGE=https://mtt.ngrok.dev/thumbnail.jpg
+NEXT_PUBLIC_APP_OG_IMAGE=\${NEXT_PUBLIC_URL}/thumbnail.jpg
 NEXT_PUBLIC_APP_TAGS="Scaffold-ETH, MiniApp, Farcaster, Base, Ponder"
 `;
